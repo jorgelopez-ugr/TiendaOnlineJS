faf3eb308db098c54fbfeb3d024e4f977ab3a9bd
J0RG170 <frijolijo17@gmail.com>
2025-10-17 13:04:04 +0200

    [rm] removed old 1.2

--- 1.2/script_backup.sh
+++
@@ -1,3 +0,0 @@
-TIMESTAMP=$(date +"%Y%m%d_%H%M")
-docker exec -it p12-mongo-1 mongodump --uri="mongodb://root:example@localhost:27017/DAI?authSource=admin" --out /backup/$TIMESTAMP
-docker cp p12-mongo-1:/backup/$TIMESTAMP ./backup/$TIMESTAMP
